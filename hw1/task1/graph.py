0cd0699274d37ad96850a350768da8beb91cfceb
陳代穎 <dying1020@gmail.com>
2018-10-06 23:53:42 +0800

    hw1-task1: Fix topological sort

---
+++ hw1/task1/graph.py
@@ -0,0 +1,76 @@
+from collections import defaultdict
+import argparse
+import numpy as np
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('graph_file')
+    return parser.parse_args()
+
+
+class Graph(object):
+    def __init__(self, graph_file):
+        self.graph = defaultdict(list)  # dictionary containing adjacency List
+        self.init_graph(graph_file)
+
+    def init_graph(self, graph_file):
+        graph = np.loadtxt(graph_file).astype(np.int32)
+        max_index = graph.max() + 1
+        self.V = max_index
+
+        self.V_seen = set()
+        for u, v in graph:
+            self.add_edge(u, v)
+            self.V_seen.update([u, v])
+
+    # function to add an edge to graph
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+
+    # A recursive function used by topologicalSort
+    def topological_sort_util(self, v, visited, stack):
+        # Mark the current node as visited.
+        visited[v] = True
+
+        # Recur for all the vertices adjacent to this vertex
+        for i in self.graph[v]:
+            if visited[i] == False:
+                self.topological_sort_util(i, visited, stack)
+
+        # Push current vertex to stack which stores result
+        stack.insert(0, v)
+
+    # The function to do Topological Sort. It uses recursive
+    # topological_sort_util()
+    def topological_sort(self):
+        # Mark all the vertices as not visited
+        visited = [False]*self.V
+        stack = []
+
+        # Call the recursive helper function to store Topological
+        # Sort starting from all vertices one by one
+        for i in range(self.V):
+            if visited[i] == False:
+                self.topological_sort_util(i, visited, stack)
+
+        # Remove unseen vertices
+        V_unseen = []
+        for v in stack:
+            if v not in self.V_seen:
+                V_unseen.append(v)
+        for v in V_unseen:
+            stack.remove(v)
+
+        return stack
+
+
+def main(args):
+    g = Graph(args.graph_file)
+    print(g.graph)
+    print(g.topological_sort())
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
--- hw1/task1/train.py
+++ hw1/task1/train.py
@@ -1,11 +1,16 @@
 import argparse
 import sys
+import logging
+from collections import defaultdict
 
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.svm import LinearSVC, SVC
+from scipy.sparse import csr_matrix
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC, LinearSVR
+
+from graph import Graph
 
 
 def parse_args():
@@ -22,6 +27,7 @@ def parse_args():
 
 
 def import_embedding(file, format):
+    logging.info('Import embedding model')
     if format == 'prune':
         embed = np.genfromtxt(file, delimiter=',')
     elif format == 'deepwalk':
@@ -42,82 +48,77 @@ def get_seen_nodes(files):
 
 
 def positive_sampling(train_file, test_seen_file, embed):
+    logging.info('Positive sampling')
     X = []
-    train_nodes = set()
+    g = Graph(train_file)
     train_graph = np.loadtxt(train_file).astype(np.int32)
     test_seen_graph = np.loadtxt(test_seen_file).astype(np.int32)
     max_index = max(train_graph.max(), test_seen_graph.max()) + 1
-    adj_matrix = np.zeros((max_index, max_index)).astype(np.int8)
+    adj_matrix = np.zeros((max_index, max_index), dtype=np.int8)
+    adj_matrix = defaultdict(set)
 
     # Sampling from train.txt
     for i, j in train_graph:
         X.append(np.concatenate((embed[i], embed[j]), axis=0))
         # Construct adjacency matrix
-        adj_matrix[int(i)][int(j)] = 1
-        """
-        if i not in adj_matrix:
-            adj_matrix[i] = set()
+        # adj_matrix[int(i)][int(j)] = 1
+        g.add_edge(i, j)
         adj_matrix[i].add(j)
-        """
-        train_nodes.update([i, j])
+
     # Sampling from test-seen.txt
     for i, j in test_seen_graph:
         X.append(np.concatenate((embed[i], embed[j]), axis=0))
         # Construct adjacency matrix
-        adj_matrix[int(i)][int(j)] = 1
-        """
-        if i not in adj_matrix:
-            adj_matrix[i] = set()
+        # adj_matrix[int(i)][int(j)] = 1
         adj_matrix[i].add(j)
-        """
 
     X = np.array(X)
     y = np.ones(X.shape[0])
-    return X, y, adj_matrix, train_nodes
+    # adj_matrix = csr_matrix(adj_matrix)
+    topo_sort = g.topological_sort()
+    return X, y, adj_matrix, topo_sort
 
 
-def negative_sampling(embed, adj_matrix, train_nodes, n_sample):
+def negative_sampling(embed, adj_matrix, topo_sort, n_sample):
+    logging.info('Negative sampling')
     X = []
     hop_matrix = adj_matrix
 
     while len(X) < int(n_sample):
-        i, j = np.random.randint(1, len(embed), 2)
-        if (i in train_nodes and j in train_nodes) and hop_matrix[i][j] == 0:
+        if len(X) % 10000 == 0: print(len(X))
+        index_i, index_j = np.random.randint(1, len(topo_sort), 2)
+        if index_i > index_j:
+            index_i, index_j = index_j, index_i
+        i, j = topo_sort[index_i], topo_sort[index_j]
+        # if hop_matrix[i][j] == 0:
+        if j not in hop_matrix[i]:
             # Add i->j to adjacency matrix to prevent duplicate negative sample
-            hop_matrix[i][j] = 0
+            # hop_matrix[i][j] = 1
+            hop_matrix[i].add(j)
             X.append(np.concatenate((embed[i], embed[j]), axis=0))
 
     X = np.array(X)
-    y = np.zeros((X.shape[0],))
+    y = np.zeros((X.shape[0]))
     return X, y
 
-    
-def avg_similarity(train_file, test_seen_file, embed):
-    X = []
-    Y = []
-    train_graph = np.loadtxt(train_file).astype(np.int32)
-    test_seen_graph = np.loadtxt(test_seen_file).astype(np.int32)
-    graph = np.concatenate((train_graph, test_seen_graph))
-    for i, j in graph:
-        X.append(embed[i])
-        Y.append(embed[j])
-    X = np.array(X)
-    Y = np.array(Y)
-    
-    sim = []
-    for x, y in zip(X, Y):
-        sim.append(cosine_similarity([x], [y])[0])
-    # sim = np.linalg.norm(X-Y, axis=1)
-    sim = np.array(sim)
-    sim = sim[sim >= 0]
-    return np.average(sim)
+
+# def nearest_neighbor_negative_sampling():
 
 
-def import_test(file, embed):
+
+def import_test(file, embed, format):
+    logging.info('Import testing data')
     X = []
     graph = np.loadtxt(file).astype(np.int32)
-    for i, j in graph:
-        X.append(np.concatenate((embed[i], embed[j]), axis=0))
+    if format == 'prune':
+        for i, j in graph:
+            X.append(np.concatenate((embed[i], embed[j]), axis=0))
+    elif format == 'deepwalk':
+        for i, j in graph:
+            if i in embed and j in embed:
+                X.append(np.concatenate((embed[i], embed[j]), axis=0))
+            else:
+                X.append(np.concatenate((np.ones(embed[1].shape), np.ones(embed[1].shape)), axis=0))
     X = np.array(X)
     return X
 
@@ -153,18 +154,24 @@ def predict_similarity(X, Y):
 
 def main(args):
     embed = import_embedding(args.embedding_file, args.embedding_file_format)
-    # avg = avg_similarity(args.train_file, args.test_seen_file, embed)
-    seen_nodes = get_seen_nodes([args.train_file, args.test_seen_file])
-    X_test, Y_test = import_test_for_similarity(args.test_file, embed, seen_nodes)
-    pred = predict_similarity(X_test, Y_test)
-    np.savetxt(args.output_file, pred, fmt='%1d')
 
     """
-    X_p, y_p, adj_matrix, train_nodes = positive_sampling(args.train_file, args.test_seen_file, embed)
-    X_n, y_n = negative_samplinsg(embed, adj_matrix, train_nodes, len(X_p)*args.negative_size)
+    Classify by similarity
+    """
+    # avg = avg_similarity(args.train_file, args.test_seen_file, embed)
+    # seen_nodes = get_seen_nodes([args.train_file, args.test_seen_file])
+    # X_test, Y_test = import_test_for_similarity(args.test_file, embed, seen_nodes)
+    # pred = predict_similarity(X_test, Y_test)
+    # np.savetxt(args.output_file, pred, fmt='%1d')
+
+    """
+    Classify by positive/negative sampling
+    """
+    X_p, y_p, adj_matrix, topo_sort = positive_sampling(args.train_file, args.test_seen_file, embed)
+    X_n, y_n = negative_sampling(embed, adj_matrix, topo_sort, len(X_p)*args.negative_size)
     X, y = np.concatenate((X_p, X_n)), np.concatenate((y_p, y_n))
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=args.test_size, shuffle=True)
-    X_test = import_test(args.test_file, embed)
+    X_test = import_test(args.test_file, embed, args.embedding_file_format)
 
     # Train classifier
     clf = LinearSVC()
@@ -173,9 +180,11 @@ def main(args):
     # Prediction
     y_test = clf.predict(X_test).astype(np.bool)
     np.savetxt(args.output_file, y_test, fmt='%1d')
-    """
+    print(y_test[y_test == 0].shape)
+    print(y_test[y_test == 1].shape)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO) 
     args = parse_args()
     main(args)
